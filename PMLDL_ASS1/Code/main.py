f1d4f36ecb635b11c2e0fb653a0f8f199c22466c
Andrei Sobolev <108589486+Barabacha474@users.noreply.github.com>
2024-09-29 19:41:34 +0300

    Dataloaders, datasets, test and val splits e.t.c.

--- PMLDL_ASS1/Code/main.py
+++ PMLDL_ASS1/Code/main.py
@@ -3,33 +3,73 @@ import tensorflow as tf
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("[*] Loading data...")
 
-    transform = transforms.Compose([
-        transforms.Grayscale(num_output_channels=1),
+    image_size = 256
+
+    train_transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=3),
+        transforms.Resize(image_size),
+        transforms.ToTensor()
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize(image_size),
         transforms.ToTensor()
     ])
 
     dataset_path = 'D:\PMLDL_ASS1\PMLDL_ASS1\Code\Datasets'
 
-    dataset = datasets.ImageFolder(root=dataset_path, transform=transform)
+    train_dataset = datasets.ImageFolder(root=dataset_path, transform=train_transform)
+    test_dataset = datasets.ImageFolder(root=dataset_path, transform=test_transform)
 
-    dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
+    print("[*] Data loaded.")
+    print("[*] Splitting dataset...")
+
+    val_ratio = 0.1
+    random_state = 200
+    indices = list(range(len(train_dataset)))
+    train_indices, val_indices = train_test_split(indices, test_size=val_ratio, random_state=random_state)
+
+    train_dataloader = DataLoader(train_indices, batch_size=128, shuffle=True)
+    val_dataloader = DataLoader(val_indices, batch_size=128, shuffle=True)
+
+    train_original = [test_dataset[index][0] for index in train_indices]
+    val_original = [test_dataset[index][0] for index in val_indices]
+
+    print("[*] Data split.")
 
     plot_image_flag = True
 
     if (plot_image_flag):
-        for batch in dataloader:
-            images, labels = batch
-            plt.imshow(images[0].numpy().transpose((1, 2, 0)))
-            plt.show()
+        print("[*] Plotting images...")
+        for batch in val_dataloader:
+            image_indexes = batch
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            rows = 3
+            fig = plt.figure(figsize=(10, 7))
+
+            for i in range(rows):
+
+                train_image = train_dataset[image_indexes[i]][0]
+                test_image = test_dataset[image_indexes[i]][0]
+
+                fig.add_subplot(rows, 2, i * 2 + 1)
+                plt.tight_layout()
+                plt.axis('off')
+                plt.title('Grayscale')
+                plt.imshow(train_image.numpy().transpose((1, 2, 0)), cmap='gray')
+
+                fig.add_subplot(rows, 2, i * 2 + 2)
+                plt.tight_layout()
+                plt.axis('off')
+                plt.title('Original')
+                plt.imshow(test_image.numpy().transpose((1, 2, 0)), cmap='gray')
+
+            plt.show()
+        print("[*] Images plotted.")
+
