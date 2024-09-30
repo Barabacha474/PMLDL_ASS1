f3487740a00dde5caef67d59727cc81f379651c8
Andrei Sobolev <108589486+Barabacha474@users.noreply.github.com>
2024-09-30 17:57:00 +0300

    NN for halucinations

--- PMLDL_ASS1/Code/main.py
+++ PMLDL_ASS1/Code/main.py
@@ -4,32 +4,34 @@ from torch import optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-import numpy as np
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
 class ColorizationNet(nn.Module):
-    def __init__(self, image_size):
+    def __init__(self, img_size, dropout_rate=0.5):
         super(ColorizationNet, self).__init__()
 
+        self.dropout_rate = dropout_rate
+
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.Conv2d(1, 32, kernel_size=3, padding=1), #256
             nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(2), # 128
+            nn.Dropout(p=self.dropout_rate),
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(2), # 64
+            nn.Dropout(p=self.dropout_rate),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(2), #32
         )
 
         self.fc_layers = nn.Sequential(
-            nn.Linear(128 * 32 * 32 + 1 * image_size * image_size, 512),
+            nn.Linear(128 * 32 * 32 + 1 * img_size * img_size, 2056),
             nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 3 * image_size * image_size)
+            nn.Dropout(p=self.dropout_rate),
+            nn.Linear(2056, 3 * img_size * img_size),
         )
 
     def forward(self, x):
@@ -47,6 +49,8 @@ class ColorizationNet(nn.Module):
         x = torch.cat([x_conv, x], dim=0)
 
         x = self.fc_layers(x)
+
+        x = torch.sigmoid(x)
 
         x = x.reshape(3, 256, 256)
 
@@ -83,16 +87,18 @@ if __name__ == '__main__':
     indices = list(range(len(train_dataset)))
     train_indices, val_indices = train_test_split(indices, test_size=val_ratio, random_state=random_state)
 
-    batch_size = 128
+    batch_size = 34
 
     train_dataloader = DataLoader(train_indices, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(val_indices, batch_size=batch_size, shuffle=True)
 
     print("[*] Data split.")
 
-    plot_image_flag = False
+    plot_load_image_flag = False
+    plot_train_image_flag = True
+    plot_val_image_flag = True
 
-    if (plot_image_flag):
+    if plot_load_image_flag:
         print("[*] Plotting images...")
         for batch in val_dataloader:
             image_indexes = batch
@@ -156,21 +162,56 @@ if __name__ == '__main__':
             model.train()
             train_loss = 0.0
 
-            for i, (img_indexes) in train_loop:
+            for k, (img_indexes) in train_loop:
 
                 optimizer.zero_grad()
 
                 outputs = []
                 test_images = []
 
-                for img_index in img_indexes:
-                    train_image = train_dataset[img_index][0].to(device)
-                    test_image = test_dataset[img_index][0].to(device)
+                plot_counter = 0
+                max_plot_counter = 1
 
-                    output = model(train_image)
+                fig_train = plt.figure(figsize=(3 * 10, max_plot_counter * 10))
+
+                for img_index in img_indexes:
+                    tr_image = train_dataset[img_index][0].to(device)
+                    tst_image = test_dataset[img_index][0].to(device)
+
+                    output = model(tr_image)
 
                     outputs.append(output)
-                    test_images.append(test_image)
+                    test_images.append(tst_image)
+
+                    if plot_train_image_flag:
+                        if plot_counter < max_plot_counter:
+                            train_grayscale_image = tr_image.clone()
+                            nn_image = output.clone()
+                            original_image = tst_image.clone()
+
+                            fig_train.add_subplot(max_plot_counter, 3, plot_counter * 3 + 1)
+                            plt.tight_layout()
+                            plt.axis('off')
+                            plt.title('Grayscale')
+                            plt.imshow(train_grayscale_image.cpu().detach().numpy().transpose((1, 2, 0)), cmap='gray')
+
+                            fig_train.add_subplot(max_plot_counter, 3, plot_counter * 3 + 2)
+                            plt.tight_layout()
+                            plt.axis('off')
+                            plt.title('NN version')
+                            plt.imshow(nn_image.cpu().detach().numpy().transpose((1, 2, 0)), cmap='gray')
+
+                            fig_train.add_subplot(max_plot_counter, 3, plot_counter * 3 + 3)
+                            plt.tight_layout()
+                            plt.axis('off')
+                            plt.title('Original')
+                            plt.imshow(original_image.cpu().detach().numpy().transpose((1, 2, 0)), cmap='gray')
+
+                            plot_counter += 1
+
+                if plot_train_image_flag:
+                    plt.title(f"Epoch {epoch}, batch {k}")
+                    plt.show()
 
                 outputs = torch.stack(outputs, dim=0)
                 test_images = torch.stack(test_images, dim=0)
@@ -189,42 +230,54 @@ if __name__ == '__main__':
             with torch.no_grad():
                 model.eval()
                 val_loop = tqdm(enumerate(val_loader, 0), total=len(val_loader), desc="Val")
-                for i, (img_indexes) in val_loop:
+                for j, (img_indexes) in val_loop:
 
                     outputs = []
                     test_images = []
 
                     plot_counter = 0
-                    max_plot_counter = 5
+                    max_plot_counter = 3
+
+                    fig_val = plt.figure(figsize=(3 * 10, max_plot_counter * 10))
 
                     for img_index in img_indexes:
-                        val_image = train_dataset[img_index][0].to(device)
-                        test_image = test_dataset[img_index][0].to(device)
+                        train_val_image = train_dataset[img_index][0].to(device)
+                        test_val_image = test_dataset[img_index][0].to(device)
 
-                        output = model(val_image)
+                        output = model(train_val_image)
 
-                        if (plot_image_flag):
-                            if (plot_counter < max_plot_counter) :
+                        if plot_val_image_flag:
+                            if plot_counter < max_plot_counter :
                                 plot_counter += 1
 
-                                fig = plt.figure(figsize=(2 * 10, 1 * 10))
+                                val_grayscale_image = train_val_image.clone()
+                                nn_image = output.clone()
+                                original_image = test_val_image.clone()
 
-                                fig.add_subplot(1, 2, 1)
+                                fig_val.add_subplot(max_plot_counter, 3, plot_counter * 3 + 1)
+                                plt.tight_layout()
+                                plt.axis('off')
+                                plt.title('Grayscale')
+                                plt.imshow(val_grayscale_image.cpu().detach().numpy().transpose((1, 2, 0)), cmap='gray')
+
+                                fig_val.add_subplot(max_plot_counter, 3, plot_counter * 3 + 2)
                                 plt.tight_layout()
                                 plt.axis('off')
                                 plt.title('NN version')
-                                plt.imshow(val_image.cpu().numpy().transpose((1, 2, 0)), cmap='gray')
+                                plt.imshow(nn_image.cpu().numpy().transpose((1, 2, 0)), cmap='gray')
 
-                                fig.add_subplot(1, 2, 2)
+                                fig_val.add_subplot(max_plot_counter, 3, plot_counter * 3 + 3)
                                 plt.tight_layout()
                                 plt.axis('off')
                                 plt.title('Original')
-                                plt.imshow(test_image.cpu().numpy().transpose((1, 2, 0)), cmap='gray')
-
-                                plt.show()
+                                plt.imshow(original_image.cpu().numpy().transpose((1, 2, 0)), cmap='gray')
 
                         outputs.append(output)
-                        test_images.append(test_image)
+                        test_images.append(test_val_image)
+
+                    if plot_val_image_flag:
+                        plt.title(f"Epoch {epoch}, batch {j}")
+                        plt.show()
 
                     outputs = torch.stack(outputs, dim=0)
                     test_images = torch.stack(test_images, dim=0)
@@ -239,7 +292,5 @@ if __name__ == '__main__':
 
 
     print("[*] Model created.")
-
-    plot_image_flag = True
 
     train(model, optimizer, loss_fn, train_dataloader, val_dataloader, epochs=num_epochs)
