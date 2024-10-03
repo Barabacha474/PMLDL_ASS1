c8a0f605d4428056dc12fdf11831cd46b48efee1
Andrei Sobolev <108589486+Barabacha474@users.noreply.github.com>
2024-10-03 22:34:40 +0300

    Image classifier

---
+++ PMLDL_ASS1/Code/Deployment/api/app.py
@@ -0,0 +1,72 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from PIL import Image
+import io
+import base64
+import torch
+import torchvision.transforms as transforms
+from Code.Models import Model
+app = FastAPI()
+
+class ImageData(BaseModel):
+    image: str
+
+@app.get("/")
+def hello():
+    """ Main page of the app. """
+    return "Hello World!"
+
+@app.post("/predict")
+async def predict_image(data: ImageData):
+    try:
+        # Decode the base64 image
+        image_data = base64.b64decode(data.image.split(',')[1])
+        image = Image.open(io.BytesIO(image_data))
+
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Resize((32,32)),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+        # Preprocess the image (resize, normalize, etc.)
+        preprocess_image = transform(image)
+
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        # Convert to PyTorch tensor and move to the appropriate device
+        input_tensor = preprocess_image.clone()
+        input_tensor = input_tensor.unsqueeze(0) # Add batch dimension
+        input_tensor = input_tensor.to(device)
+
+        model = Model.Net()
+        PATH = 'Models/Best_model.pt'
+        model.load_state_dict(torch.load(PATH, weights_only=True))
+        model.to(device)
+
+        model.eval()
+
+        # Perform inference
+        with torch.no_grad():
+            outputs = model(input_tensor)
+
+        # Get the predicted class (assuming a classification model)
+        predicted_class = torch.argmax(outputs).item()
+
+        predicted_class = model.Get_class_from_prediction(predicted_class)
+
+        # Return the prediction
+        return JSONResponse(
+            content={"prediction": predicted_class},
+            status_code=200
+        )
+
+    except Exception as e:
+        return JSONResponse(
+            content={"error": str(e)},
+            status_code=500
+        )
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
